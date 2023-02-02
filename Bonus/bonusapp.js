a8d5cde4446edb40fe060c277adc8067ef71fab5
marypaduano <maria.paduano@gmail.com>
2023-02-02 18:16:21 +0100

    bonus stampa tutto

--- Bonus/bonusapp.js
+++ Bonus/bonusapp.js
@@ -1,22 +1,21 @@
 console.log('bonus')
-let fizz
-let buzz
-let fizzBuzz
+
+let container = document.getElementById ('container')
 
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0){
         console.log("Fizz")
-        document.getElementById('container').innerHTML += 'Fizz'
+        container.innerHTML += 'Fizz'
     }
     else if (i % 5 === 0){ 
         console.log("Buzz")
-        document.getElementById('container').innerHTML += 'Buzz'
+        container.innerHTML += 'Buzz'
     }
     else if (i % 15 === 0){
         console.log("FizzBuzz")
-        document.getElementById('container').innerHTML += 'FizzBuzz'
+        container.innerHTML += 'FizzBuzz'
     }
     else console.log(i)
-    document.getElementById('container').innerHTML += (i)
+    container.innerHTML += (i)
 
 }
