62619d11eabc4f0fb643b1b22ac199d54f4dacb4
marypaduano <maria.paduano@gmail.com>
2023-02-03 09:59:39 +0100

    fix console FizzBuzz

--- Bonus/bonusapp.js
+++ Bonus/bonusapp.js
@@ -3,17 +3,17 @@ console.log('bonus')
 let container = document.getElementById ('container')
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0){
+    if (i % 15 === 0 && i % 3 === 0){
+        console.log("FizzBuzz")
+        container.innerHTML += 'FizzBuzz'
+    }
+    else if (i % 3 === 0){
         console.log("Fizz")
         container.innerHTML += 'Fizz'
     }
     else if (i % 5 === 0){ 
         console.log("Buzz")
         container.innerHTML += 'Buzz'
-    }
-    else if (i % 15 === 0){
-        console.log("FizzBuzz")
-        container.innerHTML += 'FizzBuzz'
     }
     else console.log(i)
     container.innerHTML += (i)
